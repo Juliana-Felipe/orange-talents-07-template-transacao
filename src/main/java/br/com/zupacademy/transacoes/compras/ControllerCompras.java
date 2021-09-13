c859aca950e8ddd43d83d052662a0248b799d1d0
Juliana-Felipe <juliana.felipe@zup.com.br>
2021-09-12 21:50:42 -0300

    autenticacao_autorizacao

--- src/main/java/br/com/zupacademy/transacoes/compras/ControllerCompras.java
+++ src/main/java/br/com/zupacademy/transacoes/compras/ControllerCompras.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/{id}/compras")
+@RequestMapping("/detalhamento/{id}/compras")
 public class ControllerCompras {
 
     TransacaoRepository transacaoRepository;
